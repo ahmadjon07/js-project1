05dea81932adee5cd1585fe873c21b9685809b24
ahmadjon07 <ahmadzonzulunov1@gmail.com>
2024-02-08 19:47:34 +0430

    js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var ism = prompt('Ismingizni Kiriting')
+var age = prompt('Yoshingizni Kiriting')
+var number = prompt('Raqamingizni Kiriting')
+console.log(ism);
+console.log(age);
+console.log(number);
